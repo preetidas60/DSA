cb95b82464d32d1efd11de69f1abc00e724ccda1
preetidas60 <dasp69833@gmail.com>
2026-01-18 15:38:02 +0530

    Solved GFG problem Longest Subarray with Sum K on Jan 18

---
+++ DSA-daily/dat03/MaximumSubArray.java
@@ -0,0 +1,32 @@
+import java.util.*;
+
+public class MaximumSubArray {
+    public static void main(String[] args) {
+        int[] arr = {10, 5, 2, 7, 1, -10};
+        int k = 15;
+        System.out.println(longestSubarray(arr, k));
+    }
+    public static int longestSubarray(int[] arr, int k) {
+        // code here
+        HashMap<Integer, Integer> map = new HashMap<>();
+        
+        int sum = 0;
+        int maxLen = 0;
+        for(int i=0; i<arr.length; i++){
+            sum += arr[i];
+            
+            if(sum == k){
+                maxLen = i + 1;
+            }
+            
+            if(map.containsKey(sum - k)){
+                maxLen = Math.max(maxLen, i - map.get(sum - k));
+            }
+            
+            if(!map.containsKey(sum)){
+                map.put(sum, i);
+            }
+        }
+        return maxLen;
+    }
+}
--- DSA-daily/day01/ArrayDuplicates.java
+++ DSA-daily/day01/ArrayDuplicates.java
@@ -1,11 +1,22 @@
 import java.util.ArrayList;
 import java.util.*;
+import java.util.HashMap;
 
 class ArrayDuplicates {
     public static void main(String[] args) {
         int[] arr = {1, 2, 3, 2, 4, 4, 1};
         ArrayList<Integer> duplicates = findDuplicates(arr);
         System.out.println(duplicates); 
+
+        HashMap<Integer, Integer> map = new HashMap<>();
+
+        map.put(23, 0);
+        map.put(45, 1);
+        map.put(67, 2);
+        
+        for(int i: map.values()){
+            System.out.println(i);
+        }
     }
     public static ArrayList<Integer> findDuplicates(int[] arr) {
         // code here
