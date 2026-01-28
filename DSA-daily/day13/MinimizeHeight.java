e3cef2b9aaadd1b60ac004fb4e72aa4d9fee7af1
preetidas60 <dasp69833@gmail.com>
2026-01-28 19:39:17 +0530

    Solved GFG Problem

---
+++ DSA-daily/day13/MinimizeHeight.java
@@ -0,0 +1,30 @@
+import java.util.Arrays;
+
+public class MinimizeHeight {
+    public static void main(String[] args) {
+        int[] arr = {1, 5, 8, 10};
+        int k = 2;
+        System.out.println(getMinDiff(arr, k));
+    }
+    static int getMinDiff(int[] arr, int k) {
+        int n = arr.length;
+        if (n == 1) return 0;
+
+        Arrays.sort(arr);
+
+        int ans = arr[n - 1] - arr[0];
+
+        for (int i = 1; i < n; i++) {
+
+            if (arr[i] - k < 0)
+                continue;
+
+            int minHeight = Math.min(arr[0] + k, arr[i] - k);
+            int maxHeight = Math.max(arr[i - 1] + k, arr[n - 1] - k);
+
+            ans = Math.min(ans, maxHeight - minHeight);
+        }
+
+        return ans;
+    }
+}
